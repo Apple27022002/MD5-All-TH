6fd022e9269609d63cae56e472a0c0961d14ae23
manh <v.manh2002@gmail.com>
2022-07-14 16:55:45 +0700

    xong bai calculator

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  output: number = 0;
+  first: number = 0;
+  second: number = 0;
+  operator = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  setFirst(value: any) {
+    this.first = Number(value);
+    console.log(this.first)
   }
 
+  setSecond(value: any) {
+    this.second = Number(value);
+  }
+
+  setOperator(value: any) {
+    this.operator = value;
+  }
+
+  calculate() {
+    switch (this.operator) {
+      case '+' :
+        this.output = this.first + this.second;
+        break;
+      case '-':
+        this.output = this.first - this.second;
+        break;
+      case '*':
+        this.output = this.first * this.second;
+        break;
+      case '/':
+        this.output = this.first / this.second;
+        break;
+    }
+    console.log(this.output)
+  }
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
 }
