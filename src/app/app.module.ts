7a7b92a95f41529219b4d6380c29f88d1098d623
manh <v.manh2002@gmail.com>
2022-07-14 16:10:20 +0700

    xong bai 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PetComponent } from './pet/pet.component';
 import {FormsModule} from "@angular/forms";
+import { ProductComponent } from './product/product.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PetComponent
+    PetComponent,
+    ProductComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
