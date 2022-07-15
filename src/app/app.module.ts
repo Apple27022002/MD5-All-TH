2919a4fb77204cc1e095deb67c2ae68d134b9246
manh <v.manh2002@gmail.com>
2022-07-15 11:43:11 +0700

    xong like+chia se bai moi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import {FormsModule} from "@angular/forms";
 import { ProductComponent } from './product/product.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { ColorComponent } from './color/color.component';
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ColorComponent } from './color/color.component';
     PetComponent,
     ProductComponent,
     CalculatorComponent,
-    ColorComponent
+    ColorComponent,
+    ArticleComponent,
+    LikeComponent
   ],
   imports: [
     BrowserModule,
